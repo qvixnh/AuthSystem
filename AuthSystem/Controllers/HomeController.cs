2e42792e784387b033da9af24f10e851e4e96abd
qvixnh <nguyenvinh22012002@gmail.com>
2023-09-25 19:21:17 +0700

    Get logged user id or user claims

--- AuthSystem/Controllers/HomeController.cs
+++ AuthSystem/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using AuthSystem.Models;
+﻿using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +9,17 @@ namespace AuthSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
             return View();
         }
 
